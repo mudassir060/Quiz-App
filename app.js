34d212a272b73a37ed94c1ca90077d574bcb300d
Mudassir Mukhtar <64356441+mudassir060@users.noreply.github.com>
2020-08-09 11:26:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+var score = 0
+function check(){
+    var right_awnser_1 = document.getElementById('q1-a1');
+    var q1_awnser_2 = document.getElementById('q1-a1');
+    var q1_awnser_3 = document.getElementById('q1-a1');
+    var q1_awnser_4 = document.getElementById('q1-a1');
+    alert ( 'Question No:-1 awnser is wrong')
+}
+function awnser(){
+    var right_awnser_1 = document.getElementById('q1-a1');
+    var q1_awnser_2 = document.getElementById('q1-a1');
+    var q1_awnser_3 = document.getElementById('q1-a1');
+    var q1_awnser_4 = document.getElementById('q1-a1');
+        alert ( 'Question No:-1 awnser is right')
+}
+// function  awnser(){
+//     var right_awnser_1 = document.getElementById('q1-a1');
+//     var q1_awnser_2 = document.getElementById('q1-a1');
+//     var q1_awnser_3 = document.getElementById('q1-a1');
+//     var q1_awnser_4 = document.getElementById('q1-a1');
+//     var awnser=1;
+// }
+// if (awnser==1) {
+//     score++
+//     alert ( 'Question No:-1 awnser is right')
+// } 
