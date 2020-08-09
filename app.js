5e5a9a79ac3f1933d8223f3ede1ad95889060d96
Mudassir Mukhtar <64356441+mudassir060@users.noreply.github.com>
2020-08-09 20:51:32 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 var score = 0
+var ans0 = ('Your awnser is wrong');
+var ans1 = ('Your awnser is right');
+document.getElementById("totel_result").innerHTML = "Totel score ="+"10/"+score;
+
 function check(){
     var right_awnser_1 = document.getElementById('q1-a1');
     var q1_awnser_2 = document.getElementById('q1-a1');
     var q1_awnser_3 = document.getElementById('q1-a1');
     var q1_awnser_4 = document.getElementById('q1-a1');
-    alert ( 'Question No:-1 awnser is wrong')
+    document.getElementById("ans1").innerHTML = ans0;
+    document.getElementById("q1-a1").disabled = true;
+    document.getElementById("q1-a1").disabled = true;
+    document.getElementById("q1-a2").disabled = true;
+    document.getElementById("q1-a2").disabled = true;
+    document.getElementById("q1-a3").disabled = true;
+    document.getElementById("q1-a3").disabled = true;
+    document.getElementById("q1-a4").disabled = true;
+    document.getElementById("q1-a4").disabled = true;
 }
 function awnser(){
     var right_awnser_1 = document.getElementById('q1-a1');
     var q1_awnser_2 = document.getElementById('q1-a1');
     var q1_awnser_3 = document.getElementById('q1-a1');
     var q1_awnser_4 = document.getElementById('q1-a1');
-        alert ( 'Question No:-1 awnser is right')
+    document.getElementById("ans1").innerHTML = ans1;
+    score++
+    document.getElementById("totel_result").innerHTML = "Totel score ="+"10/"+score;
+    document.getElementById("q1-a1").disabled = true;
+    document.getElementById("q1-a1").disabled = true;
+    document.getElementById("q1-a2").disabled = true;
+    document.getElementById("q1-a2").disabled = true;
+    document.getElementById("q1-a3").disabled = true;
+    document.getElementById("q1-a3").disabled = true;
+    document.getElementById("q1-a4").disabled = true;
+    document.getElementById("q1-a4").disabled = true;
 }
-// function  awnser(){
-//     var right_awnser_1 = document.getElementById('q1-a1');
-//     var q1_awnser_2 = document.getElementById('q1-a1');
-//     var q1_awnser_3 = document.getElementById('q1-a1');
-//     var q1_awnser_4 = document.getElementById('q1-a1');
-//     var awnser=1;
-// }
-// if (awnser==1) {
-//     score++
-//     alert ( 'Question No:-1 awnser is right')
-// } 
